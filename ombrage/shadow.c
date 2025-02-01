09b6d57b608101d6ac1b4913f33d58bb071ce375
Iarantsoa Evan Rabesandratana <irabesan@c1r4s2.42antananarivo.mg>
2025-02-01 14:43:04 +0300

    cylindre_top_bot=done#

--- ombrage/shadow.c
+++ ombrage/shadow.c
@@ -6,18 +6,21 @@
 /*   By: irabesan <irabesan@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 08:00:09 by mrambelo          #+#    #+#             */
-/*   Updated: 2025/01/31 15:17:28 by irabesan         ###   ########.fr       */
+/*   Updated: 2025/02/01 13:03:35 by irabesan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shadow.h"
 
-void	ray_shadowing(t_fct *fct, t_data *rt, float t)
+void	ray_shadowing(t_fct *fct, t_data *rt, t_coord *impact)
 {
-	t_coord *impact;
-	t_coord *ray_dir;
+	t_vect ray;
+	float	direction;
 
-	impact = init_coord();
-	impact = ft_addition(rt->cam->coord, ft_scal_one(fct->dir, t));
+	direction = 0.0;
+	ray.origin = &impact;
+	ray.direction = normalize_vector(ft_soustraction(rt->light->coord, impact));
+
+	
 
 }
