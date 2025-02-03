48af235a09fa30005307b8f321b2cd98863b5d4c
MarioSteven <stevenkoumar@gmail.com>
2025-02-03 14:59:54 +0300

    Cylender plan

--- ombrage/shadow.c
+++ ombrage/shadow.c
@@ -6,7 +6,7 @@
 /*   By: mrambelo <mrambelo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 08:00:09 by mrambelo          #+#    #+#             */
-/*   Updated: 2025/02/03 12:23:46 by mrambelo         ###   ########.fr       */
+/*   Updated: 2025/02/03 14:38:23 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ int intersec_lum_sphere(t_fct *fct,t_data *rt,int id,float t_lum)
 		{
 			ft_set_abc_sphere(fct,tmp,fct->origin);
 			t = get_t_sphere(fct->pol, get_delta(fct->pol));
+			printf("t  = %f\n",t);
+			printf("t_lum = %f\n\n",t_lum);
 			if (t > 0 && t < t_lum)
 				return (SHADOW);
 		}
@@ -47,7 +49,7 @@ int	ray_shadowing(t_data *rt, t_coord *impact,int id)
 	fct.origin = impact;
 	fct.pol = init_pol();
 	fct.dir = ft_soustraction(rt->light->coord, impact);
-	tmp_dir = ft_soustraction(rt->light->coord, impact);
+	tmp_dir = ft_soustraction(impact, rt->light->coord);
 	t_lum = lenght_vector(tmp_dir);
 	shadow = intersec_lum_sphere(&fct,rt,id,t_lum);
 	return(shadow);
