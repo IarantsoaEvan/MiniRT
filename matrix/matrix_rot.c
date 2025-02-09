e183c8c07fb1569088c988428fa9ee9887b790dd
StevenMario <stevenkoumar@gmail.com>
2025-02-09 19:42:59 +0300

    Corection du rotations z fait

--- matrix/matrix_rot.c
+++ matrix/matrix_rot.c
@@ -6,7 +6,7 @@
 /*   By: mrambelo <mrambelo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 10:33:33 by irabesan          #+#    #+#             */
-/*   Updated: 2025/02/07 11:43:52 by mrambelo         ###   ########.fr       */
+/*   Updated: 2025/02/09 19:42:34 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	rot_foll_x(t_coord *rot, float alpha)
 	float tmp;
 
 	tmp = rot->y;
-	printf("Avant rot->y = %f\n",rot->y);
-	printf("Avant rot->z = %f\n\n",rot->z);
+	// printf("Avant rot->y = %f\n",rot->y);
+	// printf("Avant rot->z = %f\n\n",rot->z);
 	rot->y = tmp * cos(alpha) + rot->z * sin(alpha);
 	rot->z = -(tmp) * sin(alpha) + rot->z * cos(alpha);
-	printf("Apres rot->y = %f\n",rot->y);
-	printf("Apres rot->z = %f\n\n",rot->z);
+	// printf("Apres rot->y = %f\n",rot->y);
+	// printf("Apres rot->z = %f\n\n",rot->z);
 }
 
 void	rot_foll_y(t_coord *rot, float beta)
@@ -31,7 +31,7 @@ void	rot_foll_y(t_coord *rot, float beta)
 
 	tmp = rot->x;
 	
-	printf("beta = %f\n",beta);
+	// printf("beta = %f\n",beta);
 	rot->x = tmp * cos(beta) + rot->z * sin(beta);
 	rot->z = -(tmp) * sin(beta) + rot->z * cos(beta);
 
@@ -41,12 +41,12 @@ void	rot_foll_z(t_coord *rot, float teta)
 {
 	float	tmp;
 
-	printf("teta = %f\n",teta);
-	tmp = rot->y;
-	printf("Avnat rot->x = %f\n",rot->x);
-	printf("Avnat rot->y = %f\n\n",rot->y);
+	// printf("teta = %f\n",teta);
+	tmp = rot->x;
+	// printf("Avnat rot->x = %f\n",rot->x);
+	// printf("Avnat rot->y = %f\n\n",rot->y);
 	rot->x = tmp * cos(teta) - rot->y * sin(teta);
-	rot->y = -(tmp) * sin(teta) + rot->y * cos(teta);
-	printf("Apres rot->x = %f\n",rot->x);
-	printf("Apres rot->y = %f\n\n",rot->y);
+	rot->y = tmp * sin(teta) + rot->y * cos(teta);
+// 	printf("Apres rot->x = %f\n",rot->x);
+// 	printf("Apres rot->y = %f\n\n",rot->y);
 }
