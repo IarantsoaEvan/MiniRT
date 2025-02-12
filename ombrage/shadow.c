f72fdb6eae2b2886309b83328be9632692fba74a
MarioSteven <stevenkoumar@gmail.com>
2025-02-12 10:35:37 +0300

    Ombrage cyl

--- ombrage/shadow.c
+++ ombrage/shadow.c
@@ -6,7 +6,7 @@
 /*   By: mrambelo <mrambelo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 08:00:09 by mrambelo          #+#    #+#             */
-/*   Updated: 2025/02/11 22:25:08 by mrambelo         ###   ########.fr       */
+/*   Updated: 2025/02/12 10:30:23 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,14 @@ void intersec_lum_sphere(t_fct *fct,t_data *rt,t_nearest *current,float *t_near)
 	tmp = rt->sphere;
 	// if (current->type == SPHERE)
 	// {
-	// 	printf("Miditra ato\n");
+	// 	// printf("Miditra ato\n");
 	// 	ft_set_abc_sphere(fct,sphere,fct->origin);
 	// 	te = get_t_sphere(fct->pol, get_delta(fct->pol));
 	// 	if (te > 0.001)
 	// 		return ;
 	// }
+	if (!check_self(current,fct))
+		return ;
 	while (tmp)
 	{
 		if (tmp->id != current->id)
@@ -52,6 +54,9 @@ void intersec_lum_plane(t_fct *fct,t_data *rt,t_nearest *current,float *t_near)
 	// te = get_t_plane(fct->dir,fct->origin,plane);
 	// if (te > 0.001)
 	// 	return ;
+	if (check_self(current,fct))
+		return ;
+	// printf("Tonga aty\n");
 	while (tmp)
 	{
 		if (tmp->id != current->id)
@@ -95,6 +100,8 @@ void intersec_lum_cyl(t_fct *fct,t_data *rt,t_nearest *current,float *t_near)
 	// 		return ;
 	// 	}
 	// }
+	if (!check_self(current,fct))
+		return ;
 	while (tmp)
 	{
 		// if (tmp->id == current->id)
@@ -127,34 +134,46 @@ int intersec_obj_lum(float t_lum,float t_near)
 int check_self(t_nearest *current,t_fct *fct)
 {
 	float te;
+	t_cyl *cyl;
 	// float t_top;
 	// float t_bot;
 	float t;
 	
 	
 	te = INFINITY;
+	cyl = NULL;
 	if (current->type == SPHERE)
 	{
 		ft_set_abc_sphere(fct,(t_sphere *)current->near_obj,fct->origin);
 		te = get_t_sphere(fct->pol, get_delta(fct->pol));
-		if (te < 0.001 )
+		if (te > 0.001 )
 			return(0);
 	}
 	if (current->type == CYL)
 	{
+		cyl = (t_cyl *)current->near_obj;
 		get_abc_cyl(fct->origin,fct,(t_cyl *)current->near_obj);
-		t = get_t_cyl(fct, get_delta(fct->pol), fct->origin,(t_cyl *)current->near_obj);
-		// t_top = get_base_cyl(fct->dir, fct->origin, (t_cyl *)current->near_obj,1);
-		// t_bot = get_base_cyl(fct->dir, fct->origin, (t_cyl *)current->near_obj,0);
-		if (t < 0.0001 &&  t != INFINITY)
+		t = get_t_cyl(fct, get_delta(fct->pol), fct->origin,cyl);
+		// t_top = get_base_cyl(fct->dir, fct->origin,cyl ,1);
+		// t_bot = get_base_cyl(fct->dir, fct->origin, cyl,0);
+		// t_top = get_t_plane(fct->dir,fct->origin,cyl->disk_top);
+		// t_bot = get_t_plane(fct->dir,fct->origin,cyl->disk_bot);
+		// if (t < 0.0001 && t > te)
+		// 		te = t;
+		// 	if (t_top < 0.0001 && t_top > te)
+		// 		te = t_top;
+		// 	if (t_bot < 0.0001 && t_bot > te)
+		// 		te = t_bot;
+		if (t > 0.001 && t != INFINITY)
 			return(0);
-		// if (t_top < 0.0001 )
-		// 	return(0);
-		// if (t_bot < 0.0001 )
-		// 	return(0);
-		// if (te < 0.001 && te != INFINITY)
-		// 	return(0);
 	}
+	// if (current->type == PLANE)
+	// {
+	// 	t = get_t_plane(fct->dir,fct->origin,(t_plane *)current->near_obj);
+	// 	if (t > 0.001)
+	// 		return (0);
+			
+	// }
 	return (1);
 }
 
@@ -176,10 +195,12 @@ int	ray_shadowing(t_data *rt, t_coord *impact,t_nearest *current)
 	// fct.origin = ft_scal_one(get_normal_sphere(impact,rt->sphere),0.0001);
 	fct.pol = init_pol();
 	fct.dir = normalize_vector_with_free(ft_soustraction(rt->light->coord, impact));
+	// fct.origin = vect_add(impact,ft_scal_one(fct.dir,0.001));
+
 	tmp_dir = ft_soustraction(rt->light->coord, impact);
 	t_lum = lenght_vector(tmp_dir);
-	if (check_self(current,&fct))
-		return (shadow);
+	// if (check_self(current,&fct))
+	// 	shadow = NO_SHADOW;
 	// if (current->type == SPHERE)
 	// {
 	// 	// printf("Miditra ato\n");
--- ombrage/shadow.h
+++ ombrage/shadow.h
@@ -6,7 +6,7 @@
 /*   By: mrambelo <mrambelo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 08:00:24 by mrambelo          #+#    #+#             */
-/*   Updated: 2025/02/11 13:47:48 by mrambelo         ###   ########.fr       */
+/*   Updated: 2025/02/12 10:06:24 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@
 
 
 int	ray_shadowing(t_data *rt, t_coord *impact,t_nearest *current);
-
+int check_self(t_nearest *current,t_fct *fct);
 #endif
